94ab645f96a15a40cce73f363d272695990d4d0b
Bipon Roy <biponroy400@gmail.com>
2023-10-18 13:51:58 +0600

    Added Brands name adding functionality

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const brandCollection = client.db("brandDB").collection("brands");
+
+        app.post("/brands", async (req, res) => {
+            const newBrand = req.body;
+            console.log(newBrand);
+            const result = await brandCollection.insertOne(newBrand);
+            res.send(result);
+        });
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
